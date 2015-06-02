5c8c885b0b8e65df7d860f4d066914045578f735
Deadarius <deadarius.dendr@gmail.com>
2015-06-03 08:37:57 +1200

    Updated module to match latest dashka standards

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var path = require('path');
 var fs = Promise.promisifyAll(require('fs'));
 var Handlebars = require('handlebars');
 var sass = Promise.promisifyAll(require('node-sass'));
+var mkdirp = require('mkdirp');
 
 module.exports = {
   compile: function compile(options, context, compileCallback) {
@@ -18,9 +19,10 @@ module.exports = {
     var templatePath = path.join(__dirname, 'content/layout.handlebars');
     var scssPath = path.join(__dirname, 'content/site.scss');
     function run(callback) {
+      callback = callback || _.noop;
       var renderPromises = context.getAllComponents()
         .filter(function(component) {
-          return component.$controllerName.startsWith('dashka-htmlwidget');
+          return _.startsWith(component.$controllerName, 'dashka-htmlwidget');
         })
         .map(function(component) {
           var widgetOpts = options.widgets[component.$name] || {};
@@ -47,29 +49,34 @@ module.exports = {
               };
             });
         });
-
-      Promise.all(renderPromises)
-        .then(function(widgets) {
-          var data = {
-            widgets: widgets
-          };
-          fs.read(templatePath)
-            .then(function(templateText) {
-              var template = Handlebars.compile(templateText);
-              var renderedPage = template(data);
-              var indexPath = path.join(options.outputFolder, 'index.html');
-              return fs.writeFile(indexPath, renderedPage);
-            })
-            .then(function() {
-              return sass.render({ file: scssPath });
-            })
-            .then(function(renderedScss) {
-              var cssPath = path.join(options.outputFolder, 'site.css');
-              return fs.writeFile(cssPath, renderedScss);
-            })
-            .then(function() {
-              callback();
-            });
+      var outputFolder = path.resolve(process.cwd(), options.outputFolder);
+      mkdirp(outputFolder, function(err){
+        if(err){
+          callback(err);
+        }
+        Promise.all(renderPromises)
+          .then(function(widgets) {
+            var data = {
+              widgets: widgets
+            };
+            fs.readFileAsync(templatePath, 'utf8')
+              .then(function(templateText) {
+                var template = Handlebars.compile(templateText);
+                var renderedPage = template(data);
+                var indexPath = path.join(outputFolder, 'index.html');
+                return fs.writeFileAsync(indexPath, renderedPage);
+              })
+              .then(function() {
+                return sass.renderAsync({ file: scssPath });
+              })
+              .then(function(renderedScss) {
+                var cssPath = path.join(outputFolder, 'site.css');
+                return fs.writeFileAsync(cssPath, renderedScss.css);
+              })
+              .then(function() {
+                callback();
+              });
+          });
         });
       }
       compileCallback(null, {
